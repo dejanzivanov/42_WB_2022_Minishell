4664a6d54afc0f8d8963ffcd1abdf02cef8063c9
Vlad Dragomir <vdragomi@c2s10d47.42wolfsburg.de>
2022-02-21 16:03:55 +0100

    made a safe-free utility functions and an all-rounder last values . and 0 setter function

--- srcs/ft_global_utilities.c
+++ srcs/ft_global_utilities.c
@@ -6,7 +6,7 @@
 /*   By: vdragomi <vdragomi@42student.wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 11:59:20 by vdragomi          #+#    #+#             */
-/*   Updated: 2022/02/21 13:15:06 by vdragomi         ###   ########.fr       */
+/*   Updated: 2022/02/21 16:00:39 by vdragomi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,34 @@ void	ft_last_arg(char **args, pid_t pid)
 	ft_update_create_env("_", args[i - 1], pid);
 }
 
-void	ft_set_lasts(char **args, int pid, int lreturn)
+/*
+**	if mode = 1 => we change only the $_
+**	if mode = 2 => we change only the $?
+**	if mode = 3 => we change both the $? and $_
+*/
+
+void	ft_set_lasts(char **args, int pid, int lreturn, int mode)
 {
-	if (g_access.last_return != NULL)
-		free(g_access.last_return);
-	g_access.last_return = ft_itoa(lreturn);
-	ft_last_arg(args, pid);
+	if (!(mode == 1 || mode == 2 || mode == 3))
+		write(2, "ERROR ON SET LAST\n", 18);
+	if (mode == 1)
+		ft_last_arg(args, pid);
+	else if (mode == 2)
+	{
+		if (g_access.last_return != NULL)
+			free(g_access.last_return);
+		g_access.last_return = ft_itoa(lreturn);
+	}
+	else if (mode == 3)
+	{
+		if (g_access.last_return != NULL)
+			free(g_access.last_return);
+		g_access.last_return = ft_itoa(lreturn);
+		ft_last_arg(args, pid);
+	}
 }
 
-void free_secure(void **s)
+void ft_free_secure(void **s)
 {
       if (*s != NULL)
         free(*s);
