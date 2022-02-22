680591b13b1f9060c20aa0db78013c187cdf54e2
Vlad Dragomir <vdragomi@c2s18d113.42wolfsburg.de>
2022-02-22 22:05:32 +0100

    Added first changes to cd

--- srcs/ft_builtins2.c
+++ srcs/ft_builtins2.c
@@ -1,62 +1,8 @@
 #include "../incl/minishell.h"
 
-/**
-	 @brief Bultin command: change directory.
-	 @param args List of args. args[0] is "cd". args[1] is the directory.
-	 @return Always returns 1, to continue executing.
- */
-/* int	minishell_cd(char **args, pid_t pid)
+
+static int	ft_err_cd(char **args, int pid)
 {
-	char	*temp[3];
-	char	*path;
-	DIR		*dir;
-
-	ft_update_env("_=", "cd");
-	free(g_access.last_return);
-	g_access.last_return = ft_itoa(0);
-	temp[0] = ft_strdup(args[0]);
-	temp[1] = ft_strjoin("OLDPWD=", g_access.pwd);
-	temp[2] = NULL;
-	t_list *ptr = g_access.env;
-	//IMPLEMENT ACCESS IN ORDER TO TACKLE INCORRECT PATH
-	path = ft_strdup(ft_handle_cd(args[1], ptr, pid));
-	if (path == NULL)
-		return(ft_cd_error_handler("minishell: cd: HOME not set\n", pid, &path, temp));
-	else if (!ft_strncmp(path, ";", 1))
-		return(ft_cd_error_handler("minishell: cd: OLDPWD not set\n", pid, &path, temp));
-	else if (!ft_strncmp(path, "L", 1))
-		return(ft_cd_error_handler("minishell: cd: --: invalid option\n", pid, &path, temp));
-	dir = opendir(path);
-	if ((pid == 0) && (dir == NULL))
-	{
-		free(g_access.last_return);
-		g_access.last_return = ft_itoa(1);
-		perror(ft_strjoin("minishell: cd: ", path));
-	}
-	else
-		closedir(dir);
-	if (pid != 0)
-	{
-		ft_update_create_OLDPWD(temp, ptr, pid);
-		ft_update_dir(args[1], path);
-		ft_update_PWD();
-	}
-	if (path)
-		free(path);
-	free(temp[1]);
-	free(temp[0]);
-	return (1);
-} */
-
-int minishell_cd(char **args, pid_t pid)
-{
-	char *abs_path;
-	int sym_check;
-	char *current_path;
-	DIR		*dir;
-
-	free(g_access.last_return);
-	g_access.last_return = ft_itoa(0);
 	if (args[1] == NULL)
 	{
 		if (pid == 0)
@@ -66,11 +12,12 @@ int minishell_cd(char **args, pid_t pid)
 		ft_last_arg(args, pid);
 		return(1);
 	}
-	abs_path = NULL;
-	current_path = NULL;
-	ft_rtoa_path(args[1], &abs_path);
-	dir = opendir(abs_path);
-	if (dir == NULL)
+	return (0);
+}
+
+static int	ft_dir_checker(char **args, DIR **dir, char **abs_path, int pid)
+{
+	if (*dir == NULL)
 	{
 		if (g_access.last_return != NULL)
 			free(g_access.last_return);
@@ -82,13 +29,55 @@ int minishell_cd(char **args, pid_t pid)
 			perror(" ");
 			
 		}
-		if (abs_path != NULL)
-			free(abs_path);
+		if (*abs_path != NULL)
+			free(*abs_path);
 		ft_last_arg(args, pid);
 		return (1);
 	}
 	else
-		closedir(dir);
+		closedir(*dir);
+	return (0);
+}
+/**
+	 @brief Bultin command: change directory.
+	 @param args List of args. args[0] is "cd". args[1] is the directory.
+	 @return Always returns 1, to continue executing.
+ */
+int minishell_cd(char **args, pid_t pid)
+{
+	char *abs_path;
+	int sym_check;
+	char *current_path;
+	DIR		*dir;
+
+	ft_set_lasts(args, pid, 0, 2);
+	if (ft_err_cd(args, pid) == 1)
+		return (1);
+	abs_path = NULL;
+	current_path = NULL;
+	ft_rtoa_path(args[1], &abs_path);
+	dir = opendir(abs_path);
+	if (ft_dir_checker(args, &dir, &abs_path, pid) == 1)
+		return (1);
+	// if (dir == NULL)
+	// {
+	// 	if (g_access.last_return != NULL)
+	// 		free(g_access.last_return);
+	// 	g_access.last_return = ft_itoa(1);
+	// 	if (pid == 0)
+	// 	{
+	// 		write(2, "minishell: cd: ", 15);
+	// 		write(2, args[1], ft_strlen(args[1]));
+	// 		perror(" ");
+			
+	// 	}
+	// 	if (abs_path != NULL)
+	// 		free(abs_path);
+	// 	ft_last_arg(args, pid);
+	// 	return (1);
+	// }
+	// else
+	// 	closedir(dir);
 	if (g_access.dp != NULL)
 		current_path = ft_strdup(g_access.dp);
 	else
